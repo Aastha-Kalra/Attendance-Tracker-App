8662491cd6be052413a137147288047fad795452
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-20 16:30:13 +0530

    show percentage

---
+++ src/Components/Percentage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Percentage = ({ percent, needLecture }) => {
+  return (
+    <div className="flex flex-col justify-center items-center text-3xl text-blue-800 font-bold text-pretty drop-shadow-[0_35px_35px_rgba(0,10,0,0.225)] text   ">
+      <div className="animate-bounce">Attended Percentage : {percent} %</div>
+      <div className="animate-bounce my-10">Lectures : {needLecture}</div>
+    </div>
+  );
+};
+
+export default Percentage;
