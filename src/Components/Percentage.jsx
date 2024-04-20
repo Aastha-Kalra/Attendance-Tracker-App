4cdee046bf1776502d98f56ddac49d8af13439ba
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-20 16:43:35 +0530

    updated code

--- src/Components/Percentage.jsx
+++ src/Components/Percentage.jsx
@@ -4,7 +4,7 @@ const Percentage = ({ percent, needLecture }) => {
   return (
     <div className="flex flex-col justify-center items-center text-3xl text-blue-800 font-bold text-pretty drop-shadow-[0_35px_35px_rgba(0,10,0,0.225)] text   ">
       <div className="animate-bounce">Attended Percentage : {percent} %</div>
-      <div className="animate-bounce my-10">Lectures : {needLecture}</div>
+      <div className="animate-bounce mt-10">Lectures : {needLecture}</div>
     </div>
   );
 };
